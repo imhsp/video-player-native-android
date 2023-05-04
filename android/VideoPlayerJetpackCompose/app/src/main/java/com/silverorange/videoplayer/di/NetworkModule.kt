c4c99cfee0b332bb9caf230c89613f1a7c5120f4
himanshuPatel <micky.patel41@gmail.com>
2023-05-05 00:16:04 +0530

    add retrofit hilt and create network module

---
+++ android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.silverorange.videoplayer.di
+
+import com.himansh.movielist.utils.Constants
+import com.silverorange.videoplayer.data.source.RemoteService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRepo(retrofit: Retrofit): RemoteService {
+        return retrofit.create(RemoteService::class.java)
+    }
+}
---
+++ android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.himansh.movielist.utils
+
+object Constants {
+        const val BASE_URL = "http://localhost:4000/"
+}
