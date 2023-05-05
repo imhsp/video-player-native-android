f8987d1de47c538703206f1cd6b464b7a385e269
himanshuPatel <micky.patel41@gmail.com>
2023-05-06 02:51:21 +0530

    update error screen to show appropriate error message

--- android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/ui/VideoScreen.kt
+++ android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/ui/VideoScreen.kt
@@ -1,32 +1,21 @@
 package com.silverorange.videoplayer.ui
 
-import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.TextView
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.silverorange.videoplayer.domain.mappers.ResultMap
-import io.noties.markwon.Markwon
 
 @Composable
 fun VideoScreen(videoViewModel: VideoViewModel = hiltViewModel()) {
 
-    val activity = LocalContext.current
     val state = videoViewModel.videoData.observeAsState(ResultMap.Loading)
-    val currentIndex = remember(Int) { 0 }
     when (state.value) {
         ResultMap.Loading -> {
             CircularProgressIndicator()
@@ -46,9 +35,13 @@ fun VideoScreen(videoViewModel: VideoViewModel = hiltViewModel()) {
         }
 
         is ResultMap.Failure -> {
-            Column {
-                val errorData = state.value as ResultMap.Failure
-                Text(text = "Error: ${errorData.throwable.message}")
+            Column(
+                modifier = Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Error: Loading data from URL,\n\n1. Check if local server is running\n\n2. If you are using physical device please update url in below file\n\n/android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/utils/Constants.kt")
+                Spacer(modifier = Modifier.height(10.dp))
                 Button(onClick = { videoViewModel.getVideoInfo() }) {
                     Text(text = "Retry")
                 }
--- android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/utils/Constants.kt
+++ android/VideoPlayerJetpackCompose/app/src/main/java/com/silverorange/videoplayer/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.silverorange.videoplayer.utils
 
 object Constants {
+        // Below url is set for Android emulator, if you are using physical device please update it to http://localhost:4000
         const val BASE_URL = "http://10.0.2.2:4000"
         const val PLAYER_CONTROLS_VISIBILITY = 5 * 1000L // 5 Seconds
 }
